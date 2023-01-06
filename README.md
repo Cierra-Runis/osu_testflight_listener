3fdcecdd8aaff2c5d6d85f833cb1b22e6ad383e6
Cierra-Runis <2864283875@qq.com>
2023-01-07 04:10:07 +0800

    修改📖️ & 添加🌍️ & 发现问题🐛️ 1. 修改 README.md 2. 添加 email.html 以提供网页视图 3. gmail 无法正确显示内容

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
-# osu_testflight_listener
+<div align="center">
+  <img id="osu_testflight_listener" width="96" alt="osu_testflight_listener" src="repository_icon/icon.svg">
+  <p>『 osu_testflight_listener - 内测资格！』</p>
+</div>
+
+[📚 简介](#-简介)
+
+[📦 使用方式](#-使用方式)
+
+[⏳ 进度](#-进度)
+
+[📌 注意事项](#-注意事项)
+
+[🧑‍💻 贡献者](#-贡献者)
+
+[🔦 声明](#-声明)
+
+---
+
+# 📚 简介
 
 一个为及时获取 osu 的 testflight 资格所写的小型监听器
+
+# 📦 使用方式
+
+使用 `git clone https://github.com/Cierra-Runis/osu_testflight_listener.git` 拉取至本地，按 [注意事项](#-注意事项) 添加 `email.json` 至根目录运行 `osu_testflight_listener.py` 脚本即可
+
+# ⏳ 进度
+
+已完成，按需修改
+
+# 📌 注意事项
+
+- 需添加 `email.json` 文件至根目录，内容示例如下
+
+```json
+{
+    "mail_host": "smtp.qq.com",
+    "mail_user": "********",
+    "mail_pass": "********",
+    "sender": "********@qq.com",
+    "receivers": [
+        "********@gmail.com",
+        "********@foxmail.com"
+    ]
+}
+```
+
+# 🧑‍💻 贡献者
+
+<a href="https://github.com/Cierra-Runis/osu_testflight_listener/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=Cierra-Runis/osu_testflight_listener" />
+</a>
+
+# 🔦 声明
+
+无
--- osu_testflight_listener.py
+++ osu_testflight_listener.py
@@ -3,6 +3,7 @@
 osu_testflight_listener.py
 '''
 
+import json
 import os
 import re
 import smtplib
@@ -13,6 +14,9 @@ import requests
 from alive_progress import alive_bar
 
 regex = re.compile(r'>https://testflight.apple.com/join/(.*)</a>')
+regex_html = re.compile(
+    r"<td id=\"content-cell\" align=\"left\">\n([\s\S]*?)\n                </td>"
+)
 PATH = os.path.dirname(sys.argv[0]) + r'\osu_testflight_listener.txt'
 MIN = 60  # 分钟数
 TICK = int(MIN * 6000 / 11)  # 所需 tick 数（不严谨计算）
@@ -34,44 +38,89 @@ def save_token(new_token: str):
         file.write(new_token)
 
 
-def sent_email(info):
+class Email():
+    '''
+    存储获取邮件发送配置信息
+    '''
+    # 服务器地址
+    mail_host: str
+    # 用户名
+    mail_user: str
+    # 密码或授权码
+    mail_pass: str
+    # 邮件发送方地址
+    sender: str
+    # 邮件接受方地址, 类型为 list 可以群发
+    receivers: list
+
+    def __init__(self, email_json: json) -> None:
+        # 将传入的 email_json 分配给类
+        self.mail_host = email_json['mail_host']
+        self.mail_user = email_json['mail_user']
+        self.mail_pass = email_json['mail_pass']
+        self.sender = email_json['sender']
+        self.receivers = email_json['receivers']
+
+
+def sent_email(**kwargs):
     '''
     发送邮件
     '''
-    # 服务器地址
-    mail_host = 'smtp.qq.com'
-    # 用户名
-    mail_user = '2864283875'
-    # 密码或授权码
-    mail_pass = 'urqcczoemnhrdhdi'
-    # 邮件发送方地址
-    sender = '2864283875@qq.com'
-    # 邮件接受方地址, 注意需 [] 包裹, 可以群发
-    receivers = ['a2864283875@foxmail.com']
 
-    # 设置 email 信息
-    # 邮件内容设置
-    message = MIMEText(info, 'plain', 'utf-8')
-    # 邮件主题
-    message['Subject'] = 'Osu 测试通道已更新！'
-    # 发送方信息
-    message['From'] = sender
-    # 接受方信息
-    message['To'] = receivers[0]
+    for receiver in EMAIL.receivers:
+        # 设置 email 信息
+        # 邮件内容设置
 
-    # 登录并发送邮件
-    try:
-        smtp_obj = smtplib.SMTP_SSL(mail_host)
-        # 登录到服务器
-        smtp_obj.login(mail_user, mail_pass)
-        # 发送
-        smtp_obj.sendmail(sender, receivers, message.as_string())
-        # 退出
-        smtp_obj.quit()
-        print('success')
-    except smtplib.SMTPException as error:
-        # 打印错误
-        print('error', error)
+        raw_html = open('email.html', 'r', encoding='utf-8').read()
+
+        custom_html = f'''
+            <p>你好 {receiver} ！</p>
+            <p>
+                Osu 测试通道已更新为
+                <a href="https://testflight.apple.com/join/{kwargs['info']}">
+                https://testflight.apple.com/join/{kwargs['info']}
+                </a>
+            </p>
+            <p>
+                详见
+                <a href="https://osu.ppy.sh/home/testflight">
+                https://osu.ppy.sh/home/testflight
+                </a>
+            </p>
+            <p>
+                如果您未订阅本提醒服务，请与
+                <a href="mailto:{EMAIL.sender}">服务订阅者</a>
+                联系。
+            </p>
+            '''
+        raw_html = regex_html.sub(
+            f"<td id=\"content-cell\" align=\"left\">\n{custom_html}\n                </td>",
+            raw_html,
+        )
+
+        print(raw_html)
+
+        message = MIMEText(raw_html, 'html', 'utf-8')
+        # 邮件主题
+        message['Subject'] = 'Osu 测试通道已更新！'
+        # 发送方信息
+        message['From'] = f'osu_testflight_listener <{EMAIL.sender}>'
+        # 接受方信息
+        message['To'] = receiver
+
+        # 登录并发送邮件
+        try:
+            smtp_obj = smtplib.SMTP_SSL(EMAIL.mail_host)
+            # 登录到服务器
+            smtp_obj.login(EMAIL.mail_user, EMAIL.mail_pass)
+            # 发送
+            smtp_obj.send_message(message)
+            # 退出
+            smtp_obj.quit()
+            print('success')
+        except smtplib.SMTPException as error:
+            # 打印错误
+            print('error', error)
 
 
 def wait():
@@ -95,17 +144,26 @@ def diff():
     )
     new = regex.findall(res.text)[0]
     last = get_last()
-    if last != new:
+    if True:
         print('检测到链接改变，正在发送邮件')
-        sent_email(
-            f'Osu 测试通道已更新为 https://testflight.apple.com/join/{new} ！\n' +
-            '详见 https://osu.ppy.sh/home/testflight')
+        sent_email(info=new)
         save_token(new)
     else:
         print(f'链接未改变( https://testflight.apple.com/join/{last} )')
 
 
+def insert_after(element, new_element):
+    '''
+    在元素 element 后插入 new_element
+    '''
+    parent = element.getparent()
+    parent.insert(parent.index(element) + 1, new_element)
+
+
 if __name__ == '__main__':
+    os.system('cls')
+    EMAIL = Email(json.loads(open('email.json', 'r', encoding='utf-8').read()))
+
     while True:
         try:
             diff()
